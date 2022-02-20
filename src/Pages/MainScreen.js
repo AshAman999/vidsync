c7dd253b4b0f38a687bddeaa09308fc5a9a14696
Aman Raj <amanpie9@gmail.com>
2022-02-20 14:21:52 +0530

    react player tidy up

--- src/Pages/MainScreen.js
+++ src/Pages/MainScreen.js
@@ -1,11 +1,7 @@
 import ReactPlayer from "react-player";
 import React, { useState, useRef, useEffect } from "react";
-import { IconButton } from "@material-ui/core";
-import PlayArrowIcon from "@material-ui/icons/PlayArrow";
-import PauseIcon from "@material-ui/icons/Pause";
 
 function MainScreen({ socket, username, room }) {
-  // text input controller
   const playerRef = useRef(null);
   const [text, setText] = useState("");
   const [url, seturl] = useState("https://www.youtube.com/watch?v=UVCP4bKy9Iw");
@@ -40,20 +36,16 @@ function MainScreen({ socket, username, room }) {
   useEffect(() => {
     socket.on("videourl", (data) => {
       seturl(data.url);
-      // console.log(data);
       setText("");
     });
     socket.on("play", (data) => {
       setPlaying(data.isPlaying);
-      // console.log(data);
     });
     socket.on("pause", (data) => {
       setPlaying(data.isPlaying);
-      // console.log(data);
     });
     socket.on("setPlaybackRate", (data) => {
       setPlaybackRate(data.playbackRate);
-      // console.log(data);
     });
   }, [socket]);
 
@@ -61,33 +53,30 @@ function MainScreen({ socket, username, room }) {
     <div>
       {/*TODO add video player to support playlist feature */}
       <div className="videoPlayer">
-      <ReactPlayer
-        controls={true}
-        playing={playing}
-        url={url}
-        playbackRate={playbackRate}
-        onBuffer={() => onPause()}
-        onBufferEnd={() => onPlay()}
-        onError={() => console.log("error")}
-        onEnded={() => console.log("ended")}
-        onPause={() => onPause()}
-        onPlay={() => onPlay()}
-        ref={playerRef}
-        onDuration={(duration) => console.log(duration)}
-        onProgress={(progress) => console.log(progress)}
-        onSeek={(seek) => console.log(seek)}
-        // style={{
-        //   padding: "15px",
-        //   margin: "20px",
-        // }}
-        className='react-player'
-        width='700px'
-        height='400px'
-      />
+        <ReactPlayer
+          controls={true}
+          playing={playing}
+          url={url}
+          playbackRate={playbackRate}
+          onBuffer={() => onPause()}
+          onBufferEnd={() => onPlay()}
+          onError={() => console.log("error")}
+          onEnded={() => console.log("ended")}
+          onPause={() => onPause()}
+          onPlay={() => onPlay()}
+          ref={playerRef}
+          // onDuration={(duration) => console.log(duration)}
+          // onProgress={(progress) => console.log(progress)}
+          // onSeek={(seek) => console.log(seek)}
+
+          className="react-player"
+          width="700px"
+          height="400px"
+        />
       </div>
-      <input className="urlInputBox"
+      <input
+        className="urlInputBox"
         placeholder="Enter a youtube Url"
-        
         type="text"
         value={text}
         onChange={(e) => {
@@ -97,21 +86,6 @@ function MainScreen({ socket, username, room }) {
       <button className="submitButton" onClick={() => onSubmit()}>
         Submit
       </button>
-      {/* <button
-        onClick={() => {
-          playerRef.seekTo(0);
-        }}
-      >
-        Play
-      </button>
-      <button onClick={() => onPause()}>Pause</button> */}
-      {/* <IconButton>
-        {playerRef && playerRef.current.player.isPlaying ? (
-          <PauseIcon onClick={() => onPause()} />
-        ) : (
-          <PlayArrowIcon onClick={() => onPlay()} />
-        )}
-      </IconButton> */}
     </div>
   );
 }
