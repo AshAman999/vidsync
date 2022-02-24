c0d9a64cf97764045c643e0fa2a32935774a0f96
Puja <pooja9.ps15@gmail.com>
2022-02-24 16:58:31 +0530

    enhanced css

--- src/Pages/nav.js
+++ src/Pages/nav.js
@@ -1,18 +1,17 @@
-// import React from "react";
-// import logo from "../images/logo.png";
-// import user from "../images/group-icon.png";
-// import whiteboard from "../images/white-board.png";
+import React from "react";
+import logo from "../images/logo.png";
+import user from "../images/group-icon.png";
+import whiteboard from "../images/white-board.png";
 
-// function Nav(){
-//     return(
-//             <div className="nav">
-//               <img src={logo} className="logo"/>
-//               <ul>
-//                   <li><img src = {user} className="user-img"/></li>
-//                   <li><img src = {whiteboard} className="whiteboard-img"/></li>
-//               </ul>
-//             </div>
-//     )
-// }
+function Nav(){
+    return(
+            <div className="nav">
+              <ul>
+                  <li><img src = {user} className="user-img"/></li>
+                  <li><img src = {whiteboard} className="whiteboard-img"/></li>
+              </ul>
+            </div>
+    )
+}
 
-// export default Nav;
+export default Nav;
